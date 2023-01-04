da783f2ee1f1cf72f12982423f06081e37567576
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2023-01-04 13:31:44 +0200

    1. Clock Exercise Completed.

---
+++ Nested Loops - Lab/1. Clock/Clock/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 23; i++) {
+            for (int j = 0; j <= 59; j++) {
+                System.out.println(i + ":" + j);
+            }
+        }
+    }
+}
