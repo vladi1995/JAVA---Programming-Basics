2bade6978258fa3ec99327650234b0e92f568fff
vladi1995 <105628305+vladi1995@users.noreply.github.com>
2023-01-04 14:57:14 +0200

    6. Building exercise completed.

---
+++ Nested Loops - Lab/6. Building/Building/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int floors = Integer.parseInt(scanner.nextLine());
+        int roomsPerFloor = Integer.parseInt(scanner.nextLine());
+
+        for (int i = floors; i >= 1; i--) {
+            for (int j = 0; j < roomsPerFloor; j++) {
+                if (i == floors) {
+                    System.out.print("L" + i + j + " ");
+                } else {
+                    if (i % 2 == 0) {
+                        System.out.print("O" + i + j + " ");
+                    } else {
+                        System.out.print("A" + i + j + " ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
